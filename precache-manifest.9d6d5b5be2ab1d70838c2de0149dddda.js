ca89554aabaf5f4a995b331f2dfcc46345fa574a
jeffreygatbonton <jeff@creationcrate.com>
2020-01-21 23:02:33 -0500

    Updates

---
+++ precache-manifest.9d6d5b5be2ab1d70838c2de0149dddda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb618ec97ca95de32298eec6635c7cca",
+    "url": "/react-lights-out-game/index.html"
+  },
+  {
+    "revision": "f240fc1e237d59dfecfe",
+    "url": "/react-lights-out-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8004d404fb510ddb2fb5",
+    "url": "/react-lights-out-game/static/js/2.4ceabd32.chunk.js"
+  },
+  {
+    "revision": "f240fc1e237d59dfecfe",
+    "url": "/react-lights-out-game/static/js/main.1246e3e7.chunk.js"
+  },
+  {
+    "revision": "0ab84badb07c56e949f5",
+    "url": "/react-lights-out-game/static/js/runtime~main.b925e13f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-lights-out-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
